8de9a29c95e34b4f8ae2dc686af99755580c45f3
hobinyoo <97422131+hobinyoo@users.noreply.github.com>
2023-01-06 13:34:12 +0900

    기본세팅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
 import { useEffect, useRef, useState } from 'react'
-import { css } from '@emotion/react'
-import Button from '@components/Button'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  // const [products, setProducts] = useState<
-  //   { id: string; properties: { id: string }[] }[]
-  // >([])
-  const [products, setProducts] = useState<
-    { id: string; name: string; createdAt: string }[]
-  >([])
-  // useEffect(() => {
-  //   fetch('api/get-items')
-  //     .then((res) => res.json())
-  //     .then((data) => setProducts(data.items))
-  // }, [])
-  useEffect(() => {
-    fetch('api/get-products')
-      .then((res) => res.json())
-      .then((data) => setProducts(data.items))
-  }, [])
-
-  const inputRef = useRef<HTMLInputElement>(null)
-  const handleClick = () => {
-    if (inputRef.current == null || inputRef.current.value == '') {
-      alert('name을 넣어주세요')
-      return
-    }
-    fetch(`api/add-item?name=${inputRef.current.value}`)
-      .then((res) => res.json())
-      .then((data) => alert(data.message))
-  }
   return (
     <>
       <Head>
@@ -44,65 +14,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main >
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>pages/index.tsx</code>
         </p>
-        <input
-          ref={inputRef}
-          type="text"
-          placeholder="name"
-          className="placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-        />
-        <button
-          css={css`
-            background-color: hotpink;
-            padding: 16px;
-            border-radius: 8px;
-          `}
-          onClick={handleClick}
-        >
-          Add Jacket
-        </button>
-
-        <Button onClick={handleClick}> Add Jacket2</Button>
-        <div>
-          <p>Product List</p>
-          {products &&
-            products.map((item) => (
-              <div key={item.id}>
-                {item.name}
-                <span>{item.createdAt}</span>
-              </div>
-            ))}
-          {/* {products &&
-            products.map((item) => (
-              <div key={item.id}>
-                {JSON.stringify(item)}
-                {item.properties &&
-                  Object.entries(item.properties).map(([key, value]) => (
-                    <button
-                      key={key}
-                      onClick={() => {
-                        console.log(Object.entries(item.properties))
-                        fetch(
-                          `/api/get-detail?pageId=${item.id}&propertyId=${value.id}`
-                        )
-                          .then((res) => res.json()) 
-                          .then((data) =>
-                            console.log(JSON.stringify(data.detail))
-                          )
-                      }}
-                    >
-                      {key}
-                    </button>
-                  ))}
-                <br />
-                <br />
-              </div>
-            ))} */}
-        </div>
+  
       </main>
     </>
   )
