f4624346d796d59e0ee1ddc9a46dfc09bcf763ed
hobinyoo <97422131+hobinyoo@users.noreply.github.com>
2023-01-06 15:07:35 +0900

    Design global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Global } from '@emotion/react'
 
+import { GlobalStyle } from '../styles/GlobalStyle'
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global styles={GlobalStyle}/>
+      <Component {...pageProps} />
+    </>
+  )
 }
---
+++ styles/GlobalStyle.ts
@@ -0,0 +1,37 @@
+import { css } from '@emotion/react'
+
+//GlobalStyles
+export const GlobalStyle = css`
+  :root {
+    --primary: #ffc636;
+    --secondary: #0a0b5b;
+  }
+
+  *,
+  *:after,
+  *:before {
+    box-sizing: border-box;
+  }
+
+  /* reset */
+  body,
+  p,
+  a,
+  ul,
+  li {
+    margin: 0;
+    padding: 0;
+    text-decoration: none;
+  }
+
+  li {
+    list-style-type: none;
+  }
+
+  /* base styles */
+  body {
+    background: var(--secondary);
+    overflow-x: hidden;
+  }
+
+`
